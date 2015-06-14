ac9af176d0935b996fa9ad6388798ab7e053b021
Daehyun Kim <hatemogi@gmail.com>
2015-06-14 22:33:45 +0900

    GA 추가

--- src/clojurekoans/view/index.hiccup.clj
+++ src/clojurekoans/view/index.hiccup.clj
@@ -241,4 +241,6 @@
     "주시거나, "
     [:a {:href "https://github.com/hatemogi/clojure-koans/pulls"} "풀리퀘스트를 보내"]
     "주세요."]]
-[:div.clearfix]]]
+  [:div.clearfix]]
+ [:script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-61112360-3', 'auto'); ga('send', 'pageview');"]
+ ]
